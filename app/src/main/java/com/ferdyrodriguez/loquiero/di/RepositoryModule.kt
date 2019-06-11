3a4861d12f7291b8170831ddd731257c8bcf8417
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2019-06-11 02:08:59 +0200

    Login activity and viewModel. handles login

--- app/src/main/java/com/ferdyrodriguez/loquiero/di/RepositoryModule.kt
+++ app/src/main/java/com/ferdyrodriguez/loquiero/di/RepositoryModule.kt
@@ -5,6 +5,7 @@ import com.ferdyrodriguez.data.models.ModelMapper
 import com.ferdyrodriguez.data.remote.RemoteDataSourceImpl
 import com.ferdyrodriguez.data.repositories.MainRepositoryImpl
 import com.ferdyrodriguez.domain.MainRepository
+import com.ferdyrodriguez.domain.usecases.LoginUseCase
 import com.ferdyrodriguez.domain.usecases.RegisterUserUseCase
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -13,5 +14,7 @@ val repositoryModule: Module = module {
     single { ModelMapper() }
     single { RemoteDataSourceImpl(get(), get()) as RemoteDataSource}
     single { MainRepositoryImpl(get()) as MainRepository }
+
+    factory { LoginUseCase(get()) }
     factory { RegisterUserUseCase(get()) }
 }
--- app/src/main/java/com/ferdyrodriguez/loquiero/utils/Constants.kt
+++ app/src/main/java/com/ferdyrodriguez/loquiero/utils/Constants.kt
@@ -4,6 +4,8 @@ package com.ferdyrodriguez.loquiero.utils
 class Constants {
 
     companion object{
+        const val AUTH_TOKEN = "AUTH_TOKEN"
+        const val REFRESH_TOKEN = "REFRESH_TOKEN"
         const val USER_EMAIL = "USER_EMAIL"
         const val USER_ID = "USER_ID"
     }
