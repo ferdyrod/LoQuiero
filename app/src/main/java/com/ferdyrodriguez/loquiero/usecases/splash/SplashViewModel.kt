ed1c666133a09b82b41aab1f1e2c9a1a6fa8c27c
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2019-06-12 22:25:47 +0200

    Added login or registration screen

--- app/src/main/java/com/ferdyrodriguez/loquiero/usecases/splash/SplashViewModel.kt
+++ app/src/main/java/com/ferdyrodriguez/loquiero/usecases/splash/SplashViewModel.kt
@@ -10,7 +10,7 @@ class SplashViewModel(private val verifyUseCase: VerifyUseCase,
 
     fun verifyLogin() {
         verifyUseCase(UseCase.None()) {
-            it.either({  }, {  })
+            it.either({ navigator.toLoginOrRegistration() }, {  })
 
         }
     }
