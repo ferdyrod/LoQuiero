489729f4f9a3ddfbb794dede36f308b75dcd7644
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2019-07-01 00:27:21 +0200

    integrate jacocoReport for test coverage

--- data/src/test/kotlin/com/ferdyrodriguez/data/repositories/RepositoryTests.kt
+++ data/src/test/kotlin/com/ferdyrodriguez/data/repositories/RepositoryTests.kt
@@ -50,7 +50,7 @@ class RepositoryTests : AutoCloseKoinTest() {
     @Test
     fun `should register user successfully`() {
         loadKoinModules(modules)
-        given { localDataSource.saveUserProfile("email", "password", 0, "", "") }.willReturn(Either.Right(Unit))
+
         given { remoteDataSource.getUser(0) }.willReturn(emptyUserProfileEither)
         given { remoteDataSource.registerUser("email", "password") }.willReturn(emptyRegisterEither)
         given { remoteDataSource.logInUser("email", "password") }.willReturn(emptyAuthEither)
@@ -60,8 +60,10 @@ class RepositoryTests : AutoCloseKoinTest() {
 
         verify(remoteDataSource).registerUser("email", "password")
         verify(remoteDataSource).logInUser("email", "password")
+        verify(remoteDataSource).getUser(0)
         verifyNoMoreInteractions(remoteDataSource)
         verify(localDataSource).setAuthUser(emptyAuthUser)
+        verify(localDataSource).saveUserProfile("", "", 0, "", "")
         verifyNoMoreInteractions(localDataSource)
         registeredUser.isRight shouldBe true
         registeredUser shouldEqual emptyRegisterEither
@@ -83,7 +85,7 @@ class RepositoryTests : AutoCloseKoinTest() {
     @Test
     fun `should login user successfully`() {
         loadKoinModules(modules)
-        given { localDataSource.saveUserProfile("email", "password", 0, "", "") }.willReturn(Either.Right(Unit))
+
         given { remoteDataSource.getUser(0) }.willReturn(emptyUserProfileEither)
         given { localDataSource.setAuthUser(emptyAuthUser) } willReturn { Either.Right(Unit)}
         given { remoteDataSource.logInUser("email", "password") }.willReturn(emptyAuthEither)
@@ -91,9 +93,11 @@ class RepositoryTests : AutoCloseKoinTest() {
         val loggedInUser = repository.logInUser("email", "password")
 
         verify(remoteDataSource).logInUser("email", "password")
-        //verifyNoMoreInteractions(remoteDataSource)
+        verify(remoteDataSource).getUser(0)
+        verifyNoMoreInteractions(remoteDataSource)
         verify(localDataSource).setAuthUser(emptyAuthUser)
-        //verifyNoMoreInteractions(localDataSource)
+        verify(localDataSource).saveUserProfile("", "", 0, "", "")
+        verifyNoMoreInteractions(localDataSource)
         loggedInUser.isRight shouldBe true
         loggedInUser shouldEqual emptyAuthEither
     }
